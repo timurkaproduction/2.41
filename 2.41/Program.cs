0223eb0a96ba689e017da73c939581acbeded1ba
89610 <89610@DESKTOP-VTT1R58>
2023-02-15 01:01:50 +0300

    Добавьте файлы проекта.

---
+++ 2.41/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Mainclass
+{
+    public static void Main()
+    {
+        int rad, hour, min;
+        Console.WriteLine("Введите значения угла");
+        rad = int.Parse(Console.ReadLine());
+        if (rad >= 0 && rad < 360)
+        {
+            min = (rad % 30) * 2;
+            hour = (rad - (rad % 30)) / 30;
+            Console.WriteLine($"Количество полных часов равно {hour}, минут - {min}");
+        }
+        else
+        {
+            Console.WriteLine("Введенное значение неверно");
+        }
+    }
+}
